7a1a7270c54ba525ac646d24c9c81c7377a30cf0
Likit Preeyanon <preeyano@msu.edu>
2013-07-17 11:47:52 -0400

    add count-allele.py

---
+++ count-allele.py
@@ -0,0 +1,46 @@
+import sys
+from collections import namedtuple
+
+SnpObj = namedtuple("SnpObj", ["id", "ref", "ksnp", "csnp", "info"])
+
+
+def parse_snps(snpfile):
+    for line in snpfile:
+        cols = line.split()
+        snpid = cols[2]
+        ref = cols[3]
+        ksnp = cols[4]  # known snp
+        csnp = cols[12]  # called snp
+        info = cols[16]
+    yield SnpObj(snpid, ref, ksnp, csnp, info)
+
+
+def count(snp):
+    infos = snp.info.split(";")
+    coverage = int(infos[0].split("=")[1])
+    if coverage < 10:
+        yield None
+    ref1, ref2, snp1, snp2 = infos[4].split("=")[1].split(",")
+    ref_count = int(ref1) + int(ref2)
+    snp_count = int(snp1) + int(snp2)
+    yield id, ref_count, snp_count
+
+
+def write_count(count):
+    return
+
+def main():
+    try:
+        snpfile = open(sys.argv[2])
+    except IOError:
+        print >> sys.stderr, "Cannot open %s, please check." % sys.argv[2]
+        raise SystemExit
+
+    for snp in parse_snps(snpfile):
+        id, ref_count, snp_count = count(snp)
+        print "%s\t%.4f" % (id, float(ref_count/snp_count))
+
+
+if __name__=='__main__':
+    main()
+
